f5d761b3645ad919d8ff740f1fa0c1876dd5d5f1
김용희 <52556956+dearyour@users.noreply.github.com>
2022-02-01 13:31:58 +0900

    feat: Mainfeed & friend

--- src/components/MainFeed/MainFeed.jsx
+++ src/components/MainFeed/MainFeed.jsx
@@ -18,8 +18,178 @@ function MainFeed() {
               <input id="get-image-input" type="file" />
             </div>
           </div>
+
+          <div className="feed">
+            <div className="top">
+              <div className="profile-image"></div>
+              <div className="profile-desc">
+                <dic className="nickname txt-bold">yongstar</dic>
+                <div className="timestamp">8:15 pm, yesterday</div>
+              </div>
+            </div>
+            <div className="contents">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta voluptate
+              aspernatur assumenda tenetur illum doloremque dolorum ipsa, libero explicabo nam earum
+              velit voluptatibus sunt in, illo laborum nostrum aliquid.
+            </div>
+            <div className="bottom">
+              <div className="like">
+                <div className="asset">
+                  <img src="/assets/feed/like-dac.svg" alt="좋아요" />
+                </div>
+                <div className="count" txt-bold>
+                  25k
+                </div>
+              </div>
+              <div className="comment">
+                <div className="asset">
+                  <img src="/assets/feed/comment.svg" alt="댓글" />
+                </div>
+                <div className="count" txt-bold>
+                  2k
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="feed">
+            <div className="top">
+              <div className="profile-image"></div>
+              <div className="profile-desc">
+                <dic className="nickname txt-bold">yongstar</dic>
+                <div className="timestamp">8:15 pm, yesterday</div>
+              </div>
+            </div>
+            <div className="contents">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta voluptate
+              aspernatur assumenda tenetur illum doloremque dolorum ipsa, libero explicabo nam earum
+              velit voluptatibus sunt in, illo laborum nostrum aliquid.
+              <div className="image"></div>
+            </div>
+            <div className="bottom">
+              <div className="like">
+                <div className="asset">
+                  <img src="/assets/feed/like-dac.svg" alt="좋아요" />
+                </div>
+                <div className="count" txt-bold>
+                  25k
+                </div>
+              </div>
+              <div className="comment">
+                <div className="asset">
+                  <img src="/assets/feed/comment.svg" alt="댓글" />
+                </div>
+                <div className="count" txt-bold>
+                  2k
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="feed">
+            <div className="top">
+              <div className="profile-image"></div>
+              <div className="profile-desc">
+                <dic className="nickname txt-bold">yongstar</dic>
+                <div className="timestamp">8:15 pm, yesterday</div>
+              </div>
+            </div>
+            <div className="contents">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta voluptate
+              aspernatur assumenda tenetur illum doloremque dolorum ipsa, libero explicabo nam earum
+              velit voluptatibus sunt in, illo laborum nostrum aliquid.
+            </div>
+            <div className="bottom">
+              <div className="like">
+                <div className="asset">
+                  <img src="/assets/feed/like-dac.svg" alt="좋아요" />
+                </div>
+                <div className="count" txt-bold>
+                  25k
+                </div>
+              </div>
+              <div className="comment">
+                <div className="asset">
+                  <img src="/assets/feed/comment.svg" alt="댓글" />
+                </div>
+                <div className="count" txt-bold>
+                  2k
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="feed">
+            <div className="top">
+              <div className="profile-image"></div>
+              <div className="profile-desc">
+                <dic className="nickname txt-bold">yongstar</dic>
+                <div className="timestamp">8:15 pm, yesterday</div>
+              </div>
+            </div>
+            <div className="contents">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta voluptate
+              aspernatur assumenda tenetur illum doloremque dolorum ipsa, libero explicabo nam earum
+              velit voluptatibus sunt in, illo laborum nostrum aliquid.
+              <div className="image"></div>
+            </div>
+            <div className="bottom">
+              <div className="like">
+                <div className="asset">
+                  <img src="/assets/feed/like-dac.svg" alt="좋아요" />
+                </div>
+                <div className="count" txt-bold>
+                  25k
+                </div>
+              </div>
+              <div className="comment">
+                <div className="asset">
+                  <img src="/assets/feed/comment.svg" alt="댓글" />
+                </div>
+                <div className="count" txt-bold>
+                  2k
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="friend-list"></div>
+        <div className="friend-list">
+          <div className="my-profile">
+            <div className="profile-image"></div>
+            <div className="nickname" txt-bold>
+              yongstar
+            </div>
+          </div>
+          <div className="my-friends">
+            <div className="title txt-bold">나의 친구</div>
+            <ul className="friend-list-wrapper">
+              <li className="friend">
+                <div className="profile-image"></div>
+                <div className="nickname" txt-bold>
+                  일주어터
+                </div>
+              </li>
+              <li className="friend">
+                <div className="profile-image"></div>
+                <div className="nickname" txt-bold>
+                  카리나
+                </div>
+              </li>
+              <li className="friend">
+                <div className="profile-image"></div>
+                <div className="nickname" txt-bold>
+                  윈터
+                </div>
+              </li>
+              <li className="friend">
+                <div className="profile-image"></div>
+                <div className="nickname" txt-bold>
+                  랭커
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
